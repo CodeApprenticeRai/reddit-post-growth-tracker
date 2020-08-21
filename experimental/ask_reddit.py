9b283924fb83000e3efa04660426892d37d3ce0e
tare.dgg@gmail.com <tare.dgg@gmail.com>
2020-08-21 17:21:48 -0400

    ask-reddit analyzer specification

--- experimental/ask_reddit.py
+++ experimental/ask_reddit.py
@@ -2,10 +2,18 @@
 AskReddit is a high volume subreddit.
 A batch of many new questions are posted,
 all day, everyday.
-There a good bit of variation between posts to.
+There a good bit of variation between posts too.
 
 Reddit doesn't let you visualize the growth of
 these posts, and so it'd be cool to see the different
 classes of posts based on how they grow.
 
+Spec:
+* Register every new submission as soon as it is posted ( check every 30 seconds, or if possible be notified )
+* Every 30s for every registered submission record it's stats at the current time
+* allow for posts to be queried based on this info:
+    * show a graph of all posts
+
+That's it. Just being able to see the growth curves of new posts.
+Being able to see what's on people's minds. 
 '''
