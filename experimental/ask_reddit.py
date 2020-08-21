210859825ca45a093a7b6f22fead0e05631d6900
tare.dgg@gmail.com <tare.dgg@gmail.com>
2020-08-21 17:11:23 -0400

    ask reddit motive

---
+++ experimental/ask_reddit.py
@@ -0,0 +1,11 @@
+'''
+AskReddit is a high volume subreddit.
+A batch of many new questions are posted,
+all day, everyday.
+There a good bit of variation between posts to.
+
+Reddit doesn't let you visualize the growth of
+these posts, and so it'd be cool to see the different
+classes of posts based on how they grow.
+
+'''
